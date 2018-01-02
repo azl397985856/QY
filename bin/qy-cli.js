297d95c12f75af7d339757e857533baaed6800b0
luzhipeng <luzhipeng@duiba.com.cn>
2018-01-02 10:57:12 +0800

    first commit

---
+++ bin/qy-cli.js
@@ -0,0 +1,74 @@
+#!/usr/bin/env node
+
+var program = require("commander");
+var fs = require("fs");
+
+var imgCore = require("../src/imgCore");
+var depCore = require("../src/depCore");
+
+var mail = require("../src/mail");
+var webhook = require("../src/webhook");
+
+program
+  .version(require("../package.json").version)
+  .usage("[project name] [options] ")
+  .option(
+    "-C, --config <str>",
+    "specify your config file, default is .qyconfig"
+  )
+  .parse(process.argv);
+
+var deploy_env = program.args[0];
+var config = program.config ? program.config : ".qyconfig";
+fs.readFile(config, function(err, data) {
+  if (!err) {
+    var config = JSON.parse(data.toString("utf-8"));
+    if (config) {
+      // 1. 图片：包括图片类型检测，图片大小检测，图片相似度检测。
+      // loading
+      var imgPromise = imgCore
+        .check(config)
+        .then(res => {
+          // loading finish
+          if (res.success) {
+            // chalk green
+          } else {
+            // chalk red
+          }
+        })
+        .catch(err => {
+          // loading finish
+          // chalk red
+        });
+      // 2. 依赖：对项目的 package 进行检测，发现没用的，过期的，是否有已知的安全隐患。
+      var depPromise = depCore
+        .check(config)
+        .then(res => {
+          // loading finish
+          if (res.success) {
+            // chalk green
+          } else {
+            // chalk red
+          }
+        })
+        .catch(err => {
+          // loading finish
+          // chalk red
+        });
+
+      Promise.all(imgPromise, depPromise)
+        .then(res => {
+          // mail or post webhook
+          webhook.send(config);
+          mail.send(config);
+        })
+        .catch(err => {
+          // mail or post webhook
+          webhook.send(config);
+          mail.send(config);
+        });
+    }
+  } else {
+    console.error("config file", config, "is not found");
+  }
+});
